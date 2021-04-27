86417fd101c1151c82a55470d01cb4bc9b40e057
Catarina  Munn <>
2021-04-26 20:23:08 -0400

    Create function to check players input and capture input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
     const placeholder = [];
@@ -19,9 +20,48 @@ const placeholder = function (word) {
     wordProgress.innerText = placeholderLettters.join("");
 }; 
 
+
+
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
     const guess = letterInput.value; 
-    console.log(guess);
-    letterInput.value = "";
+    message.innerText = "";
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+      }
+      letterInput.value = "";
 });
+
+
+
+const validateInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input === 0){
+        message.innerText = "Please enter a letter";
+    } else if (input.length > 1){
+    message.innerText = "Enter a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z!";
+    } else {
+        return input;
+    }
+
+};
+
+const makeGuess = function(guess){
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter";
+    } else  {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+
+    }
+};
+
+
+
+
+
